2d1fcc18175ceced3e83825d9619e8dcf2941d21
Brenno Maia <66637868+brennomaia@users.noreply.github.com>
2021-06-09 19:34:28 -0300

    Atualizando lendo email

--- received_email.py
+++ received_email.py
@@ -20,21 +20,29 @@ server.login(email_from, email_passwd)
 #Criando uma lista de ids com todos emails contidos em bot_gmud
 #Separando os Ids(emails) da lista e criando uma nova lista com o split
 server.select("bot_gmud")   
-result, id_email = server.uid('search', None, "ALL")    
-id_item_email = id_email[0].split()
+result, id_email = server.search(None, 'ALL')   
+email_id = id_email[0].split()
+
+#email_id = []
+#for block in id_email:
+    #email_id += block.split()
+
 
 #Trazendo o id"email" mais recente
-id_email_recent = id_item_email[-1]
+id_email_recent = email_id[0]
 
 #Trazendo o id"mail" mais antigo
-id_email_old = id_item_email[0]
+#id_email_old = email_id[0]
 
 #Lendo o conteudo do ultimo email em bytes
 #convertendo o email em bytes para utf-8
-result2, email_data = server.uid('fetch', id_email_recent, '(RFC822)')
-utf_email = email_data[0][1].decode("utf-8")
-email_message = email.message_from_string(utf_email)
-print(email_message)
+result2, email_data = server.fetch(id_email_recent, '(RFC822)')
+_, b = email_data[0]
+message = email.message_from_bytes(b)
+for part in message.walk():
+    if part.get_content_type() == "text/plain" or part.get_content_type() == "text/html":
+        body = part.get_payload(decode=True)
+        print(body)
 
 
 
